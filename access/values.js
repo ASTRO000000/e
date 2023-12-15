e07921ec070c8ab1bc35f9b9da0a057ae44e7845
ASTRO000000 <153936139+ASTRO000000@users.noreply.github.com>
2023-12-15 19:57:47 +0200

    Add files via upload

---
+++ access/values.js
@@ -0,0 +1,11 @@
+// Активировать документы (true - включены, false - отключены)
+var isRightsEnabled = true; // Права
+var isCovidCertificateEnabled = true; // COVID-сертификат
+
+// Общее
+var birthdate = "24.03.2005"; // Дата рождения
+var fullname = "Дідик Костянтин Олександрович" // ФИО
+var name = "Костянтин" // Имя
+
+// Паспорт
+var passport_id = "739275497" // Номер паспорта
